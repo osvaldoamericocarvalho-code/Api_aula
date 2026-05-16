f998a91a5bdbc18e7de2696d6fc67bb3a9cfac88
osvaldoamericocarvalho-code <osvaldoamericocarvalho@gmail.com>
2026-05-16 11:39:03 -0300

    Add FastAPI endpoint for summing two numbers

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 # Passando o número 1 e 2 na URL
+from fastapi import FastAPI
+
+app = FastAPI()
+
 @app.get("/soma/{numero1}/{numero2}")
 def soma(numero1: int, numero2: int):
     total = numero1 + numero2
