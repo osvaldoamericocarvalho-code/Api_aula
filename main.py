6223c370f8ba89b4c1e0b6b094e74185b269e1c0
osvaldoamericocarvalho-code <osvaldoamericocarvalho@gmail.com>
2026-05-16 14:17:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -13,3 +13,15 @@ def soma(numero1: int, numero2: int):
 def soma_formato2(numero1: int, numero2: int):
     total = numero1 + numero2
     return {"resultado": total}
+
+
+# Passando o número 1 e 2 no corpo da requisição
+from pydantic import BaseModel
+class Numeros(BaseModel):
+    numero1: int
+    numero2: int
+
+@app.post("/soma_formato3")
+def soma_formato3(numeros: Numeros):
+    total = numeros.numero1 + numeros.numero2
+    return {"resultado": total}
